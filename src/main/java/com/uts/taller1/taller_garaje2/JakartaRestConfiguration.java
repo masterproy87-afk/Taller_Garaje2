9df5952aa7a86cdb20eae46aff9191e159617c6c
angel <angel@ANGIE>
2025-09-25 18:46:16 -0500

    carga inicial

---
+++ src/main/java/com/uts/taller1/taller_garaje2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.uts.taller1.taller_garaje2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
